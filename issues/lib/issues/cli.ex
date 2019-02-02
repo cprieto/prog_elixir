8acd729555e55dcc08f45d49584cb0b9339f09d1
Cristian Prieto <me@cprieto.com>
2019-02-02 15:39:26 +0100

    Now we decode json

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   """
 
   def run(args) do
-    parse_args(args)
+    args
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -24,17 +26,32 @@ defmodule Issues.CLI do
     )
     |> elem(1)
     |> args_to_internal_repr
+    |> process
   end
 
-  def args_to_internal_repr([user, project, count]) do
-    {user, project, String.to_integer(count)}
+  def args_to_internal_repr([user, project, count]), do: {user, project, String.to_integer(count)}
+
+  def args_to_internal_repr([user, project]), do: {user, project, @default_count}
+
+  def args_to_internal_repr(_), do: :help
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count |#{@default_count} ]
+    """
+
+    System.halt(0)
   end
 
-  def args_to_internal_repr([user, project]) do
-    {user, project, @default_count}
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project) |> decode_response
   end
 
-  def args_to_internal_repr(_) do
-    :help
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
   end
+
+  def decode_response({:ok, body}), do: body
+
 end
---
+++ issues/lib/issues/github_issues.ex
@@ -0,0 +1,26 @@
+defmodule Issues.GithubIssues do
+  @github_url Application.get(:issues, :github_url)
+  @user_agent [{"User-agent", "Elixir test@example.com"}]
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def check_for_error(200), do: :ok
+  def check_for_error(_), do: :error
+
+  def handle_response(_, %{status_code: status_code, body: body}) do
+    {
+      status_code
+      |> check_for_error,
+      body
+      |> Poison.Parser.parse!()
+    }
+  end
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+end
